176e10014515555d6c3728505546419689f3b8aa
israr anjum <israranjum851@gmail.com>
2021-10-30 04:11:15 +0500

    Who am I Added

--- services/CustomErrorHandler.js
+++ services/CustomErrorHandler.js
@@ -7,7 +7,20 @@ class CustomErrorHandler extends Error{
     static alreadyExist(message){
         return new CustomErrorHandler(409,message);
     }
+
+    static wrongCredentials(message = 'Username or password is wrong!'){
+        return new CustomErrorHandler(401,message);
+    }
+
+    static unAuthorized(message = 'unAuthorized'){
+        return new CustomErrorHandler(401, message);
+    }
+
+    static notFound(message = '404 Not Found'){
+        return new CustomErrorHandler(404, message);
+    }
 }
+
 
 
 export default CustomErrorHandler
